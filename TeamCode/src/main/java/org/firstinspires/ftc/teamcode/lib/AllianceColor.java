cee4aa9da2c26e0edc3b39783b533387ecd63b47
Siddharth Ray <sidraystudios@gmail.com>
2022-12-12 12:31:36 -0800

    added distance/color sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/AllianceColor.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.lib;
+
+public enum AllianceColor {
+    RED,
+    BLUE,
+    BOTH
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/claw/ClawSensor.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.subsystems.claw;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.lib.AllianceColor;
+
+public class ClawSensor {
+    ColorSensor sensor;
+
+    // Constants of the cone colors
+    int redColor = 0;
+    int blueColor = 0;
+
+    DistanceUnit unit = DistanceUnit.CM;
+
+    public ClawSensor(ColorSensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public int getRawARGB() {
+        return sensor.argb();
+    }
+
+    public boolean conePresent(AllianceColor color) {
+        int sensorValue = sensor.argb();
+
+        switch (color) {
+            case RED:
+                if (sensorValue == redColor) {
+                    return true;
+                }
+            case BLUE:
+                if (sensorValue == blueColor) {
+                    return true;
+                }
+        }
+
+        // Return false if color provided is BOTH or if the color was not detected
+        return false;
+    }
+
+    public double getRange() {
+        return ((DistanceSensor) sensor).getDistance(unit);
+    }
+
+
+}
