2cea547e49c23153253d3bad7d9170f426e07327
Siddharth Ray <sidraystudios@gmail.com>
2023-01-20 20:40:37 -0800

    added manual controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.java
@@ -82,10 +82,28 @@ public class TeleOpMain extends LinearOpMode {
 //                robot.resetAllServos();
 //            }
 
-            if (gamepad2.right_stick_x > 0.2) {
-                robot.startClawY(true);
-            } else if (gamepad2.right_stick_x < -0.2) {
-                robot.startClawY(false);
+            if (gamepad2.left_trigger > 0.1) {
+                robot.slides.runToPosition((int) (robot.slides.slides1.motor.getCurrentPosition() + (0.1*gamepad2.left_trigger)));
+            } else if (gamepad2.right_trigger > 0.1) {
+                robot.slides.runToPosition((int) (robot.slides.slides1.motor.getCurrentPosition() - (0.1*gamepad2.left_trigger)));
+            }
+
+            if (gamepad2.left_bumper) {
+                robot.v4b.setAngle(robot.v4b.currentAngle - 0.1);
+            } else if (gamepad2.right_bumper) {
+                robot.v4b.setAngle(robot.v4b.currentAngle + 0.1);
+            }
+
+            if (gamepad2.dpad_up) {
+                robot.claw.setXRotation((float) (robot.claw.clawX1.getAngle() + 0.1));
+            } else if (gamepad2.dpad_down) {
+                robot.claw.setXRotation((float) (robot.claw.clawX1.getAngle() - 0.1));
+            }
+
+            if (gamepad2.dpad_right) {
+                robot.claw.clawY.setAngle((float) (robot.claw.clawY.getAngle() + 0.1));
+            } else if (gamepad2.dpad_right) {
+                robot.claw.clawY.setAngle((float) (robot.claw.clawY.getAngle() - 0.1));
             }
 
 
