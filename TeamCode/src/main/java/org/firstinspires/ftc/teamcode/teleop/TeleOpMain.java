ddcec6ecd72523197c164c61ed6e05ec9a7db7c4
Siddharth Ray <sidraystudios@gmail.com>
2023-02-13 11:59:48 -0800

    advisory boring af so finished auto-skill issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.java
@@ -12,6 +12,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveCancelable;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @TeleOp(group = "competition")
 public class TeleOpMain extends LinearOpMode {
@@ -29,6 +30,7 @@ public class TeleOpMain extends LinearOpMode {
         MODE mode = MODE.DRIVER;
         PhotonCore.enable();
         autonDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Pose2d startPose = new Pose2d(0, 0, 0);
 
         double x;
         double y;
@@ -36,6 +38,26 @@ public class TeleOpMain extends LinearOpMode {
 
         boolean previousClawSensorState = false;
         ElapsedTime clawSensorTimeout = new ElapsedTime();
+
+        TrajectorySequence autonCycle = autonDrive.trajectorySequenceBuilder(startPose)
+                .addDisplacementMarker(() -> {
+                    boolean coneSeen = false;
+                    while (!coneSeen) {
+                        if (robot.claw.sensor.conePresent()) {
+                            coneSeen = true;
+                        }
+                    }
+                    robot.claw.setClawClose();
+                    robot.highPreset(true);
+                })
+                .back(10)
+                .addTemporalMarker(5, () -> {
+                    robot.advancedToggleClaw();
+                    robot.groundPreset(true);
+                })
+                .waitSeconds(2)
+                .forward(10)
+                .build();
 
         Gamepad previousGamepad1 = new Gamepad();
         Gamepad previousGamepad2 = new Gamepad();
@@ -58,98 +80,104 @@ public class TeleOpMain extends LinearOpMode {
 
             switch (mode) {
                 case DRIVER:
-                if (gamepad1.dpad_up) {
-                    robot.slides.resetAllEncoders();
-                }
+                    if (gamepad1.dpad_up) {
+                        robot.slides.resetAllEncoders();
+                    }
 
-                //DRIVE
-                if (gamepad1.right_trigger > 0.1) {
-                    x = -gamepad1.left_stick_x * (1 - 0.66 * gamepad1.right_trigger);
-                    y = -gamepad1.left_stick_y * (1 - 0.66 * gamepad1.right_trigger);
-                    rx = gamepad1.right_stick_x * (1 - 0.66 * gamepad1.right_trigger);
+                    //DRIVE
+                    if (gamepad1.right_trigger > 0.1) {
+                        x = -gamepad1.left_stick_x * (1 - 0.66 * gamepad1.right_trigger);
+                        y = -gamepad1.left_stick_y * (1 - 0.66 * gamepad1.right_trigger);
+                        rx = gamepad1.right_stick_x * (1 - 0.66 * gamepad1.right_trigger);
 
-                } else {
-                    x = -gamepad1.left_stick_x;
-                    y = -gamepad1.left_stick_y;
-                    rx = gamepad1.right_stick_x;
-                }
-                robot.setDrivePower(-x, y, rx);
+                    } else {
+                        x = -gamepad1.left_stick_x;
+                        y = -gamepad1.left_stick_y;
+                        rx = gamepad1.right_stick_x;
+                    }
+                    robot.setDrivePower(-x, y, rx);
 
-                //PRESETS
-                if (gamepad1.right_bumper)
-                    robot.groundPreset(gamepad1.dpad_down);
-                if (gamepad1.dpad_left)
-                    robot.lowPreset(gamepad1.dpad_left);
-                if (gamepad1.dpad_right)
-                    robot.mediumPreset(gamepad1.dpad_right);
-                if (gamepad1.left_bumper)
-                    robot.highPreset(gamepad1.left_bumper);
-                if (gamepad1.dpad_down)
-                    robot.sidewaysPickup(gamepad1.dpad_down);
+                    //PRESETS
+                    if (gamepad1.right_bumper)
+                        robot.groundPreset(gamepad1.dpad_down);
+                    if (gamepad1.dpad_left)
+                        robot.lowPreset(gamepad1.dpad_left);
+                    if (gamepad1.dpad_right)
+                        robot.mediumPreset(gamepad1.dpad_right);
+                    if (gamepad1.left_bumper)
+                        robot.highPreset(gamepad1.left_bumper);
+                    if (gamepad1.dpad_down)
+                        robot.sidewaysPickup(gamepad1.dpad_down);
 
-                //CLAW
-                if (gamepad1.circle && !autoClosePreviousState) {
-                    autoCloseEnabled = !autoCloseEnabled;
-                }
+                    if (gamepad1.triangle) {
+                        gamepad1.rumble(200);
+                        autonDrive.followTrajectorySequenceAsync(autonCycle);
+                        mode = MODE.AUTON;
+                    }
 
-                if (gamepad2.left_trigger > 0.1) {
-                    robot.slides.runToPosition((int) (robot.slides.slides1.motor.getCurrentPosition() + (0.1 * gamepad2.left_trigger)));
-                } else if (gamepad2.right_trigger > 0.1) {
-                    robot.slides.runToPosition((int) (robot.slides.slides1.motor.getCurrentPosition() - (10 * gamepad2.left_trigger)));
-                }
+                    //CLAW
+                    if (gamepad1.circle && !autoClosePreviousState) {
+                        autoCloseEnabled = !autoCloseEnabled;
+                    }
 
-                if (gamepad2.left_bumper) {
-                    robot.v4b.setAngle(robot.v4b.currentAngle - 1);
-                } else if (gamepad2.right_bumper) {
-                    robot.v4b.setAngle(robot.v4b.currentAngle + 1);
-                }
+                    if (gamepad2.left_trigger > 0.1) {
+                        robot.slides.runToPosition((int) (robot.slides.slides1.motor.getCurrentPosition() + (0.1 * gamepad2.left_trigger)));
+                    } else if (gamepad2.right_trigger > 0.1) {
+                        robot.slides.runToPosition((int) (robot.slides.slides1.motor.getCurrentPosition() - (10 * gamepad2.left_trigger)));
+                    }
 
-                if (gamepad2.dpad_up) {
-//                robot.claw.setXRotation((float) (robot.claw.clawX1.getAngle() + 1));
-                } else if (gamepad2.dpad_down) {
-//                robot.claw.setXRotation((float) (robot.claw.clawX1.getAngle() - 1));
-                }
+                    if (gamepad2.left_bumper) {
+                        robot.v4b.setAngle(robot.v4b.currentAngle - 1);
+                    } else if (gamepad2.right_bumper) {
+                        robot.v4b.setAngle(robot.v4b.currentAngle + 1);
+                    }
 
-                if (gamepad2.dpad_right) {
-                    robot.claw.clawY.setAngle((float) (robot.claw.clawY.getAngle() + 1));
-                } else if (gamepad2.dpad_right) {
-                    robot.claw.clawY.setAngle((float) (robot.claw.clawY.getAngle() - 1));
-                }
+                    if (gamepad2.dpad_up) {
+    //                robot.claw.setXRotation((float) (robot.claw.clawX1.getAngle() + 1));
+                    } else if (gamepad2.dpad_down) {
+    //                robot.claw.setXRotation((float) (robot.claw.clawX1.getAngle() - 1));
+                    }
 
-                boolean isPressed = gamepad1.cross;
-                if (isPressed && !previousClawState) {
-                    robot.advancedToggleClaw();
-                }
-                previousClawState = isPressed;
+                    if (gamepad2.dpad_right) {
+                        robot.claw.clawY.setAngle((float) (robot.claw.clawY.getAngle() + 1));
+                    } else if (gamepad2.dpad_right) {
+                        robot.claw.clawY.setAngle((float) (robot.claw.clawY.getAngle() - 1));
+                    }
 
-                if (!previousClawSensorState && robot.claw.sensor.conePresent() && clawSensorTimeout.milliseconds() >= 50) {
-                    gamepad1.rumble(500);
-                    robot.claw.toggle();
-                }
+                    boolean isPressed = gamepad1.cross;
+                    if (isPressed && !previousClawState) {
+                        robot.advancedToggleClaw();
+                    }
+                    previousClawState = isPressed;
 
-                if (previousClawSensorState && !robot.claw.sensor.conePresent() && clawSensorTimeout.milliseconds() >= 50) {
-                    clawSensorTimeout.reset();
-                }
-//
-//            if (gamepad1.circle) {
-//                robot.resetAllServos();
-//            }
+                    if (!previousClawSensorState && robot.claw.sensor.conePresent() && clawSensorTimeout.milliseconds() >= 50) {
+                        gamepad1.rumble(500);
+                        robot.claw.toggle();
+                    }
 
-                if (gamepad2.right_stick_x > 0.2) {
-                    robot.startClawY(true);
-                } else if (gamepad2.right_stick_x < -0.2) {
-                    robot.startClawY(false);
-                }
+                    if (previousClawSensorState && !robot.claw.sensor.conePresent() && clawSensorTimeout.milliseconds() >= 50) {
+                        clawSensorTimeout.reset();
+                    }
+    //
+    //            if (gamepad1.circle) {
+    //                robot.resetAllServos();
+    //            }
 
-                boolean isPressed2 = gamepad1.square;
-                if (gamepad1.square && !previousRetractState) {
-                    robot.toggleRetract();
-                }
-                previousRetractState = isPressed2;
+                    if (gamepad2.right_stick_x > 0.2) {
+                        robot.startClawY(true);
+                    } else if (gamepad2.right_stick_x < -0.2) {
+                        robot.startClawY(false);
+                    }
 
-                previousClawSensorState = robot.claw.sensor.conePresent();
-                autoClosePreviousState = gamepad1.circle;
-                break;
+                    boolean isPressed2 = gamepad1.square;
+                    if (gamepad1.square && !previousRetractState) {
+                        robot.toggleRetract();
+                    }
+                    previousRetractState = isPressed2;
+
+                    previousClawSensorState = robot.claw.sensor.conePresent();
+                    autoClosePreviousState = gamepad1.circle;
+                    break;
                 case AUTON:
                     // Update the drive class
                     autonDrive.update();
@@ -163,9 +191,9 @@ public class TeleOpMain extends LinearOpMode {
                         mode = MODE.DRIVER;
                     }
 
-                    // If drive finishes its task, cede control to the driver
+                    // If drive finishes its task, repeat cycle
                     if (!autonDrive.isBusy()) {
-                        mode = MODE.DRIVER;
+                        autonDrive.followTrajectorySequenceAsync(autonCycle);
                     }
 
                     if (gamepad1.left_stick_x != 0 || gamepad1.left_stick_y != 0 || gamepad1.right_stick_x != 0 || gamepad1.right_stick_y != 0) {
