4b3ad6ce97f2b7947ca5d249db192aaef6fdb0f2
Siddharth Ray <58677791+kidsonfilms-python-rules@users.noreply.github.com>
2023-04-16 12:02:55 -0700

    create cv master
    i am ur master

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/CVMaster.java
@@ -0,0 +1,177 @@
+package org.firstinspires.ftc.teamcode.subsystems.vision;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public class CVMaster {
+    private OpenCvWebcam webcam;
+    private Robot robot;
+    private StickObserverPipeline opencv = null;
+    private ConeObserverPipeline cone = null;
+    private AprilTagDetectionPipeline aprilTags = null;
+
+    private boolean isStreaming = false;
+
+    public CVMaster(HardwareMap hardwareMap){
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+    }
+
+    public boolean isStreaming(){
+        return isStreaming;
+    }
+
+    public void observeStick(){
+        opencv = new StickObserverPipeline();
+        webcam.setPipeline(opencv);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
+                 * supports streaming from the webcam in the uncompressed YUV image format. This means
+                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
+                 *
+                 * Also, we specify the rotation that the webcam is used in. This is so that the image
+                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
+                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+                 * away from the user.
+                 */
+                webcam.setPipeline(opencv);
+                webcam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+
+                FtcDashboard.getInstance().startCameraStream(webcam,10);
+
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+        isStreaming = true;
+    }
+
+    public void observeCone(){
+        cone = new ConeObserverPipeline();
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
+                 * supports streaming from the webcam in the uncompressed YUV image format. This means
+                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
+                 *
+                 * Also, we specify the rotation that the webcam is used in. This is so that the image
+                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
+                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+                 * away from the user.
+                 */
+                webcam.setPipeline(cone);
+//                webcam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+        isStreaming = true;
+    }
+  
+  public void observeSleeve(){
+        aprilTags = new AprilTagDetectionPipeline();
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
+                 * supports streaming from the webcam in the uncompressed YUV image format. This means
+                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
+                 *
+                 * Also, we specify the rotation that the webcam is used in. This is so that the image
+                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
+                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+                 * away from the user.
+                 */
+                webcam.setPipeline(aprilTags);
+                webcam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+                dashboard.startCameraStream(webcam, 10);
+
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+            }
+        });
+        isStreaming = true;
+
+    }
+  
+    public int getSleevePark(){
+        return aprilTags.getLatestDetections();
+    }
+
+    public double centerOfPole(){
+        return opencv.centerOfPole();
+    }
+
+    public double poleSize(){
+        return opencv.poleSize();
+    }
+
+    public double[] rotatedPolarCoord(){
+        return opencv.poleRotatedPolarCoord();
+    }
+
+    public double[] rotatedConarCoord(){return cone.coneRotatedPolarCoord();}
+
+    public void stopCamera(){
+        webcam.stopRecordingPipeline();
+        webcam.stopStreaming();
+        webcam.stopStreaming();
+    }
+
+}
